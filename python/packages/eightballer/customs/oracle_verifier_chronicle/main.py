c3a9f8a88965b04ed285b563c74ca8d154f0e817
8baller <8ball030@gmail.com>
2024-07-13 22:31:30 +0100

    hashes

--- python/packages/eightballer/customs/oracle_verifier_chronicle/main.py
+++ python/packages/eightballer/customs/oracle_verifier_chronicle/main.py
@@ -7,13 +7,20 @@ from aea.skills.base import Behaviour
 # we use web3 to make some calls to the Ethereum node.
 from web3 import Web3
 
-w3 = Web3(Web3.HTTPProvider("http://https://rpc.ankr.com/eth_sepolia")
+w3 = Web3(Web3.HTTPProvider("http://https://rpc.ankr.com/eth_sepolia"))
 
 
-def collect_data() -> None:
+def collect_data(web3=w3, address='0xdd6D76262Fd7BdDe428dcfCd94386EbAe0151603' ) -> None:
     """
     Collect data from the oracle verifier chronicle.
     """
+    # get the contract ABI
+    contract_abi = Path("packages/eightballer/customs/oracle_verifier_chronicle/abi.json").read_text()
+    contract_address = address
+    contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+    # get the data from the contract
+    data = contract.functions.get_data().call()
+    return data
 
 
 def verify_data() -> None:

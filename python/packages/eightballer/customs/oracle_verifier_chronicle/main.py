c78723605adf24ce752731e95e301d6e5f757607
8baller <8ball030@gmail.com>
2024-07-13 22:09:32 +0100

    cleanup

--- python/packages/eightballer/customs/oracle_verifier_chronicle/main.py
+++ python/packages/eightballer/customs/oracle_verifier_chronicle/main.py
@@ -4,6 +4,10 @@ from typing import cast
 from pathlib import Path
 
 from aea.skills.base import Behaviour
+# we use web3 to make some calls to the Ethereum node.
+from web3 import Web3
+
+w3 = Web3(Web3.HTTPProvider("http://https://rpc.ankr.com/eth_sepolia")
 
 
 def collect_data() -> None:
@@ -11,13 +15,11 @@ def collect_data() -> None:
     Collect data from the oracle verifier chronicle.
     """
 
-    raise NotImplementedError("This function is not implemented yet.")
 
 def verify_data() -> None:
     """
     Verify data from the oracle verifier chronicle.
     """
-
     raise NotImplementedError("This function is not implemented yet.")
 
 def challenge_data() -> None:

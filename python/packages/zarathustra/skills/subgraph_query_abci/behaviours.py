ca86ddd6a1160d19f9937de57b414ddf26e8ba75
Karrenbelt <karrenbelt_git@pm.me>
2024-07-13 19:10:55 +0200

    feat: templated subgraph_query_abci

---
+++ python/packages/zarathustra/skills/subgraph_query_abci/behaviours.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2024 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This package contains round behaviours of SubgraphQueryAbciApp."""
+
+from abc import ABC
+from typing import Generator, Set, Type, cast
+
+from packages.valory.skills.abstract_round_abci.base import AbstractRound
+from packages.valory.skills.abstract_round_abci.behaviours import (
+    AbstractRoundBehaviour,
+    BaseBehaviour,
+)
+
+from packages.zarathustra.skills.subgraph_query_abci.models import Params
+from packages.zarathustra.skills.subgraph_query_abci.rounds import (
+    SynchronizedData,
+    SubgraphQueryAbciApp,
+    CheckSubgraphsHealthRound,
+    CollectSubgraphsDataRound,
+    DataTransformationRound,
+    LoadSubgraphComponentsRound,
+)
+from packages.zarathustra.skills.subgraph_query_abci.rounds import (
+    CheckSubgraphsHealthPayload,
+    CollectSubgraphsDataPayload,
+    DataTransformationPayload,
+    LoadSubgraphComponentsPayload,
+)
+
+
+class SubgraphQueryBaseBehaviour(BaseBehaviour, ABC):
+    """Base behaviour for the subgraph_query_abci skill."""
+
+    @property
+    def synchronized_data(self) -> SynchronizedData:
+        """Return the synchronized data."""
+        return cast(SynchronizedData, super().synchronized_data)
+
+    @property
+    def params(self) -> Params:
+        """Return the params."""
+        return cast(Params, super().params)
+
+
+class CheckSubgraphsHealthBehaviour(SubgraphQueryBaseBehaviour):
+    """CheckSubgraphsHealthBehaviour"""
+
+    matching_round: Type[AbstractRound] = CheckSubgraphsHealthRound
+
+    # TODO: implement logic required to set payload content for synchronization
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            sender = self.context.agent_address
+            payload = CheckSubgraphsHealthPayload(sender=sender, content=...)
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+
+class CollectSubgraphsDataBehaviour(SubgraphQueryBaseBehaviour):
+    """CollectSubgraphsDataBehaviour"""
+
+    matching_round: Type[AbstractRound] = CollectSubgraphsDataRound
+
+    # TODO: implement logic required to set payload content for synchronization
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            sender = self.context.agent_address
+            payload = CollectSubgraphsDataPayload(sender=sender, content=...)
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+
+class DataTransformationBehaviour(SubgraphQueryBaseBehaviour):
+    """DataTransformationBehaviour"""
+
+    matching_round: Type[AbstractRound] = DataTransformationRound
+
+    # TODO: implement logic required to set payload content for synchronization
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            sender = self.context.agent_address
+            payload = DataTransformationPayload(sender=sender, content=...)
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+
+class LoadSubgraphComponentsBehaviour(SubgraphQueryBaseBehaviour):
+    """LoadSubgraphComponentsBehaviour"""
+
+    matching_round: Type[AbstractRound] = LoadSubgraphComponentsRound
+
+    # TODO: implement logic required to set payload content for synchronization
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            sender = self.context.agent_address
+            payload = LoadSubgraphComponentsPayload(sender=sender, content=...)
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+
+class SubgraphQueryRoundBehaviour(AbstractRoundBehaviour):
+    """SubgraphQueryRoundBehaviour"""
+
+    initial_behaviour_cls = LoadSubgraphComponentsBehaviour
+    abci_app_cls = SubgraphQueryAbciApp  # type: ignore
+    behaviours: Set[Type[BaseBehaviour]] = [
+        CheckSubgraphsHealthBehaviour,
+        CollectSubgraphsDataBehaviour,
+        DataTransformationBehaviour,
+        LoadSubgraphComponentsBehaviour
+    ]
---
+++ python/packages/zarathustra/skills/subgraph_query_abci/rounds.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2024 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This package contains the rounds of SubgraphQueryAbciApp."""
+
+from enum import Enum
+from typing import Dict, FrozenSet, List, Optional, Set, Tuple
+
+from packages.valory.skills.abstract_round_abci.base import (
+    AbciApp,
+    AbciAppTransitionFunction,
+    AbstractRound,
+    AppState,
+    BaseSynchronizedData,
+    DegenerateRound,
+    EventToTimeout,
+)
+
+from packages.zarathustra.skills.subgraph_query_abci.payloads import (
+    CheckSubgraphsHealthPayload,
+    CollectSubgraphsDataPayload,
+    DataTransformationPayload,
+    LoadSubgraphComponentsPayload,
+)
+
+
+class Event(Enum):
+    """SubgraphQueryAbciApp Events"""
+
+    MAX_RETRIES = "max_retries"
+    SYNCHRONIZED = "synchronized"
+    DONE = "done"
+    RETRY = "retry"
+
+
+class SynchronizedData(BaseSynchronizedData):
+    """
+    Class to represent the synchronized data.
+
+    This data is replicated by the tendermint application.
+    """
+
+
+class CheckSubgraphsHealthRound(AbstractRound):
+    """CheckSubgraphsHealthRound"""
+
+    payload_class = CheckSubgraphsHealthPayload
+    payload_attribute = ""  # TODO: update
+    synchronized_data_class = SynchronizedData
+
+    # TODO: replace AbstractRound with one of CollectDifferentUntilAllRound,
+    # CollectSameUntilAllRound, CollectSameUntilThresholdRound,
+    # CollectDifferentUntilThresholdRound, OnlyKeeperSendsRound, VotingRound,
+    # from packages/valory/skills/abstract_round_abci/base.py
+    # or implement the methods
+
+    def end_block(self) -> Optional[Tuple[BaseSynchronizedData, Enum]]:
+        """Process the end of the block."""
+        raise NotImplementedError
+
+    def check_payload(self, payload: CheckSubgraphsHealthPayload) -> None:
+        """Check payload."""
+        raise NotImplementedError
+
+    def process_payload(self, payload: CheckSubgraphsHealthPayload) -> None:
+        """Process payload."""
+        raise NotImplementedError
+
+
+class CollectSubgraphsDataRound(AbstractRound):
+    """CollectSubgraphsDataRound"""
+
+    payload_class = CollectSubgraphsDataPayload
+    payload_attribute = ""  # TODO: update
+    synchronized_data_class = SynchronizedData
+
+    # TODO: replace AbstractRound with one of CollectDifferentUntilAllRound,
+    # CollectSameUntilAllRound, CollectSameUntilThresholdRound,
+    # CollectDifferentUntilThresholdRound, OnlyKeeperSendsRound, VotingRound,
+    # from packages/valory/skills/abstract_round_abci/base.py
+    # or implement the methods
+
+    def end_block(self) -> Optional[Tuple[BaseSynchronizedData, Enum]]:
+        """Process the end of the block."""
+        raise NotImplementedError
+
+    def check_payload(self, payload: CollectSubgraphsDataPayload) -> None:
+        """Check payload."""
+        raise NotImplementedError
+
+    def process_payload(self, payload: CollectSubgraphsDataPayload) -> None:
+        """Process payload."""
+        raise NotImplementedError
+
+
+class DataTransformationRound(AbstractRound):
+    """DataTransformationRound"""
+
+    payload_class = DataTransformationPayload
+    payload_attribute = ""  # TODO: update
+    synchronized_data_class = SynchronizedData
+
+    # TODO: replace AbstractRound with one of CollectDifferentUntilAllRound,
+    # CollectSameUntilAllRound, CollectSameUntilThresholdRound,
+    # CollectDifferentUntilThresholdRound, OnlyKeeperSendsRound, VotingRound,
+    # from packages/valory/skills/abstract_round_abci/base.py
+    # or implement the methods
+
+    def end_block(self) -> Optional[Tuple[BaseSynchronizedData, Enum]]:
+        """Process the end of the block."""
+        raise NotImplementedError
+
+    def check_payload(self, payload: DataTransformationPayload) -> None:
+        """Check payload."""
+        raise NotImplementedError
+
+    def process_payload(self, payload: DataTransformationPayload) -> None:
+        """Process payload."""
+        raise NotImplementedError
+
+
+class LoadSubgraphComponentsRound(AbstractRound):
+    """LoadSubgraphComponentsRound"""
+
+    payload_class = LoadSubgraphComponentsPayload
+    payload_attribute = ""  # TODO: update
+    synchronized_data_class = SynchronizedData
+
+    # TODO: replace AbstractRound with one of CollectDifferentUntilAllRound,
+    # CollectSameUntilAllRound, CollectSameUntilThresholdRound,
+    # CollectDifferentUntilThresholdRound, OnlyKeeperSendsRound, VotingRound,
+    # from packages/valory/skills/abstract_round_abci/base.py
+    # or implement the methods
+
+    def end_block(self) -> Optional[Tuple[BaseSynchronizedData, Enum]]:
+        """Process the end of the block."""
+        raise NotImplementedError
+
+    def check_payload(self, payload: LoadSubgraphComponentsPayload) -> None:
+        """Check payload."""
+        raise NotImplementedError
+
+    def process_payload(self, payload: LoadSubgraphComponentsPayload) -> None:
+        """Process payload."""
+        raise NotImplementedError
+
+
+class FailedToSynchronizeRound(DegenerateRound):
+    """FailedToSynchronizeRound"""
+
+
+class FinalSubgraphQueryRound(DegenerateRound):
+    """FinalSubgraphQueryRound"""
+
+
+class SubgraphQueryAbciApp(AbciApp[Event]):
+    """SubgraphQueryAbciApp"""
+
+    initial_round_cls: AppState = LoadSubgraphComponentsRound
+    initial_states: Set[AppState] = {LoadSubgraphComponentsRound}
+    transition_function: AbciAppTransitionFunction = {
+        LoadSubgraphComponentsRound: {
+            Event.DONE: CheckSubgraphsHealthRound
+        },
+        CheckSubgraphsHealthRound: {
+            Event.SYNCHRONIZED: CollectSubgraphsDataRound,
+            Event.RETRY: CheckSubgraphsHealthRound,
+            Event.MAX_RETRIES: FailedToSynchronizeRound
+        },
+        CollectSubgraphsDataRound: {
+            Event.DONE: DataTransformationRound
+        },
+        DataTransformationRound: {
+            Event.DONE: FinalSubgraphQueryRound
+        },
+        FailedToSynchronizeRound: {},
+        FinalSubgraphQueryRound: {}
+    }
+    final_states: Set[AppState] = {FinalSubgraphQueryRound, FailedToSynchronizeRound}
+    event_to_timeout: EventToTimeout = {}
+    cross_period_persisted_keys: FrozenSet[str] = frozenset()
+    db_pre_conditions: Dict[AppState, Set[str]] = {
+        LoadSubgraphComponentsRound: {'participants'},
+    }
+    db_post_conditions: Dict[AppState, Set[str]] = {
+        FinalSubgraphQueryRound: {'most_voted_tx_hash'},
+    	FailedToSynchronizeRound: [],
+    }

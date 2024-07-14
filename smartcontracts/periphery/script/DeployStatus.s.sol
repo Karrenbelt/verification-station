b65661db82d922b741c39cafcdc28e5127db3b22
Niels Launert <niels@blockwerft.de>
2024-07-14 04:44:14 +0200

    Revise Status deploy contract

--- smartcontracts/periphery/script/DeployStatus.s.sol
+++ smartcontracts/periphery/script/DeployStatus.s.sol
@@ -7,15 +7,21 @@ import {Script} from "forge-std/Script.sol";
 import {Status} from "../src/Status.sol";
 
 contract Deploy is Script {
-    function deploy() public returns (address statusAddress) {
+    function deploy(address _owner) public returns (address statusAddress) {
         vm.startBroadcast();
-        Status status = new Status();
+        Status status = new Status(_owner);
         vm.stopBroadcast();
 
         return address(status);
     }
 
     function run() external returns (address statusAddress) {
-        return deploy();
+        try vm.envAddress("OWNER") returns (address owner) {
+            return deploy(owner);
+        } catch {
+            revert(
+                "Please sent OWNER along with the script command:\n    make deployStatus ARGS='...' OWNER='0x...'"
+            );
+        }
     }
 }

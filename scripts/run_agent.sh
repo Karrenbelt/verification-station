ac20b71b430d7a32a585c90eeb93c7b119bdf253
8baller <8ball030@gmail.com>
2024-07-14 01:40:30 +0100

    fix-http-routing

--- scripts/run_agent.sh
+++ scripts/run_agent.sh
@@ -11,19 +11,19 @@ TARGET_FOLDER="$REPO_ROOT/python/oracle_verifier"
 [ -d "$TARGET_FOLDER" ] && rm -rf "$TARGET_FOLDER"
 
 # Fetch the agent from the local packages directory
-aea fetch zarathustra/oracle_verifier --local
+aea -s fetch zarathustra/oracle_verifier --local
 
 # Navigate to the project directory
 cd oracle_verifier
 
 # Install agent dependencies
-aea install
+aea -s install
 
 # Create a key for the agent
-aea generate-key ethereum && aea add-key ethereum
+aea -s generate-key ethereum && aea -s add-key ethereum
 
 # Issue certificates for the Libp2p connection
-aea issue-certificates
+aea -s issue-certificates
 
 # and run the agent
-aea run
+aea -s run
